778bdf7461e3325ecc02f6efba56d893538031da
Omar <omarezz03@gmail.com>
2025-03-06 01:30:28 +0200

    added developer entity

---
+++ TwinBackend.Solution/TwinBackend.Core/Entities/Developer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwinBackend.Core.Entities
+{
+    public class Developer : BaseEntity
+    {
+        public bool isActive;
+        public bool isWorking;
+        public ProjectTask? CurrentTask;
+        public double SkillTestScore;
+        public string GithubLink;
+        public List<string> Titles;
+        public List<string> Skills;
+        public int RejectedOffersCounter;
+        public int MissedOffersCounter;
+        public Offer? CurrentOffer;
+        public List<ProjectTask> FinishedTasks;
+        public double AvgTasksRating;
+        public List<bool> HassAccessToMarketplace;
+        public bool IsConfirmed;
+    }
+}
---
+++ TwinBackend.Solution/TwinBackend.Repository/Data/Repositories/DeveloperRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwinBackend.Core.Entities;
+
+namespace TwinBackend.Repository.Data.Repositories
+{
+    public class DevelperRepository : GenericRespository<Question>
+    {
+        public DevelperRepository(TwinDbContext context) : base(context) { }
+    }
+}
