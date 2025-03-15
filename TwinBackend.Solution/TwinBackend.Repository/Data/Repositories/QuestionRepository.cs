3b607e9e0fa9892c46b590806cfdc6973537c152
markayman15 <markayman453@gmail.com>
2025-03-15 21:19:42 +0200

    Add Questions

--- TwinBackend.Solution/TwinBackend.Repository/Data/Repositories/QuestionRepository.cs
+++ TwinBackend.Solution/TwinBackend.Repository/Data/Repositories/QuestionRepository.cs
@@ -4,10 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TwinBackend.Core.Entities;
+using TwinBackend.Core.Repositories.Contract;
 
 namespace TwinBackend.Repository.Data.Repositories
 {
-    public class QuestionRepository : GenericRespository<Question>
+    public class QuestionRepository : GenericRespository<Question>, IGenericRepository<Question>
     {
         public QuestionRepository(TwinDbContext context) : base(context) { }
     }
