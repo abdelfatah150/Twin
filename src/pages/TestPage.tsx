1b137285d485b349dcb4f3621458abab3ea5424c
Youssef <youssefkaramedu@gmail.com>
2025-03-22 22:50:14 +0200

    add basic test

--- src/pages/TestPage.tsx
+++ src/pages/TestPage.tsx
@@ -1,11 +1,27 @@
 import React from "react";
 import StandardPage from "../Components/StandardPage";
-import "../styles/TestPage.css"
+import "../styles/TestPage.css";
 
 const TestPage: React.FC = () => {
   return (
     <StandardPage>
-      <h1>Test Page</h1>
+      <div className="test-page">
+        <div className="test-container">
+          <div className="left-column">
+            <h2>Backend</h2>
+            <p>Questions: 45</p>
+            <button className="start-btn">Start Now</button>
+          </div>
+          <div className="right-column">
+            <h1>Developer Skill Assessment - Determine Your Level</h1>
+            <ul>
+              <li>Django - 15 Questions</li>
+              <li>OOP - 15 Questions</li>
+              <li>Data Structure - 15 Questions</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </StandardPage>
   );
 };
